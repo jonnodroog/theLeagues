acf4478f30b8a3276266738cc62256eafb87cbf4
Jonathan Draai <jonnodraai@gmail.com>
2025-04-14 21:12:14 +0200

    configured home api

---
+++ HomeAPI/Endpoints/LeagueEndpoints.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HomeAPI.LeagueEndpoints
+{
+    public static class LeagueEndpoints
+    {
+        public static void RegisterLeagueEndpoints(this IEndpointRouteBuilder routes)
+        {
+            var leagueEndpointsGroup = routes.MapGroup("/leagues");
+        }
+    }
+}
---
+++ HomeAPI/Extensions/Configuration.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace HomeAPI.Extensions
+{
+    public static class Configuration
+    {
+        public static void RegisterServicees(this WebApplicationBuilder builder)
+        {
+            #region Configure environment specific settings
+            #endregion
+
+            #region Rate limiting
+                builder.Services.AddRateLimiter(_ => _
+                .AddFixedWindowLimiter(policyName: "fixed", options =>
+                {
+                    options.PermitLimit = 4;
+                    options.Window = TimeSpan.FromSeconds(12);
+                    options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                    options.QueueLimit = 10;
+                }));
+            #endregion
+
+            #region Declare custom services
+            #endregion
+
+            #region Swagger
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddOpenApiDocument(config =>
+                {
+                    config.DocumentName = "HomeAPI";
+                    config.Title = "HomeAPI V1";
+                    config.Version = "V1";
+                });
+            #endregion
+
+            #region Configure CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin",
+                    builder => builder.AllowAnyOrigin()
+                                      .AllowCredentials()
+                                      .AllowAnyHeader());
+            });
+            #endregion
+
+            #region AuthN/AuthZ
+            #endregion
+        }
+        public static void RegisterMiddlewares(this WebApplication app)
+        {
+                app.UseHttpsRedirection();
+
+                app.UseCors("AllowAnyOrigin");
+
+                app.UseExceptionHandler();
+                //app.UseAuthorization();
+
+                if(app.Environment.IsDevelopment())
+                {
+                   app.UseOpenApi();
+                   app.UseSwaggerUi(config =>
+                   {
+                    config.DocumentTitle = "HomeAPI";
+                    config.Path = "/swagger";
+                    config.DocExpansion = "/swagger/{documentName}/swagger.json";
+                    config.DocExpansion = "list";
+                   });
+                }
+
+                app.UseRateLimiter();
+        }
+    }
+}
