151001874d73107c43051d343c9747df34c41712
Jonathan Draai <jonnodraai@gmail.com>
2025-04-14 22:50:14 +0200

    db stuff

--- Models/obj/Debug/net7.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net7.0/Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a2087e69ad3d15f5fecdeeca903b00c136d003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a3bc2bad9e51bd55309bb05bc5d4e8d49b5d64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
